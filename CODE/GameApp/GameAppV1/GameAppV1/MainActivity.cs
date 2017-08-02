e27cb4a6500d0a0201500d8413d7560e6bf2fd02
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-08-02 14:32:53 +1200

    save changes

--- CODE/GameApp/GameAppV1/GameAppV1/MainActivity.cs
+++ CODE/GameApp/GameAppV1/GameAppV1/MainActivity.cs
@@ -5,11 +5,18 @@ using System;
 
 namespace GameAppV1
 {
-    [Activity(Label = "DSED-02 Assignment", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "DSED-02 Assignment GameAppV1", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
 
         ImageView ImageWordSlot01;
+
+        ImageView ImageTile01;
+        ImageView ImageTile02;
+        ImageView ImageTile03;
+        ImageView ImageTile04;
+        ImageView ImageTile05;
+        ImageView ImageTile06;
 
         Button btnPressMe;
 
@@ -28,22 +35,52 @@ namespace GameAppV1
             InitializeControl();
         }
 
+
         private void InitializeControl()
         {
             ImageWordSlot01 = FindViewById<ImageView>(Resource.Id.ImageWordSlot01);
 
             btnPressMe = FindViewById<Button>(Resource.Id.btnPressMe);
+            ImageTile01 = FindViewById<ImageView>(Resource.Id.ImageTile01);
+            ImageTile02 = FindViewById<ImageView>(Resource.Id.ImageTile02);
+            ImageTile03 = FindViewById<ImageView>(Resource.Id.ImageTile03);
+            ImageTile04 = FindViewById<ImageView>(Resource.Id.ImageTile04);
+            ImageTile05 = FindViewById<ImageView>(Resource.Id.ImageTile05);
+            ImageTile06 = FindViewById<ImageView>(Resource.Id.ImageTile06);
 
             index = 0;
-
+            char ch = 'B';
             btnPressMe.Click += (send, e) =>
+            //{
+            //    string resource = "word_" + Letter_List[index];
+
+            //    ImageWordSlot01.SetImageResource((int)typeof(Resource.Drawable).GetField(resource).GetValue(null));
+
+            //    if (++index == Letter_List.Length)
+            //        index = 0;
+            //};
             {
-                string resource = "word_" + Letter_List[index];
-
-                ImageWordSlot01.SetImageResource((int)typeof(Resource.Drawable).GetField(resource).GetValue(null));
-
-                if (++index == Letter_List.Length)
+                if(index == 6)
+                {
                     index = 0;
+                    ch = (ch == 'B' ? 'A' : 'B');
+                }
+
+                ImageView image;
+
+                switch(index + 1)
+                {
+                    case 1: image = ImageTile01;  break;
+                    case 2: image = ImageTile02; break;
+                    case 3: image = ImageTile03; break;
+                    case 4: image = ImageTile04; break;
+                    case 5: image = ImageTile05; break;
+                    case 6: image = ImageTile06; break;
+                    default: throw new InvalidOperationException("ImageView not defined !!!");
+                }
+
+                string resource = "hangman_tile_0"+ ++index + "_" + ch;
+                image.SetImageResource((int)typeof(Resource.Drawable).GetField(resource).GetValue(null));
             };
         }
     }
