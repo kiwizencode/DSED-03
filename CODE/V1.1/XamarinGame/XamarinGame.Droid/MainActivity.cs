a0986444f63848bc031100471110bd783491af15
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-08-21 09:26:53 +1200

    save project

---
+++ CODE/V1.1/XamarinGame/XamarinGame.Droid/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace XamarinGame.Droid
+{
+    [Activity(Label = "XamarinGame.Droid", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        Button btnStartGame;
+        Button btnScores;
+        Button btnProfile;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.main_layout);
+
+            btnStartGame = FindViewById<Button>(Resource.Id.btnStartGame);
+            btnStartGame.Click += delegate
+            {
+                var newactivity = new Intent(this, typeof(GameActivity));
+
+                newactivity.PutExtra("Activity_Name", "Game Activity");
+                StartActivity(newactivity);
+
+            };
+
+            btnProfile = FindViewById<Button>(Resource.Id.btnProfile);
+            btnProfile.Click += delegate
+            {
+                var newactivity = new Intent(this, typeof(ProfileActivity));
+
+                newactivity.PutExtra("Activity_Name", "Profile Activity");
+                StartActivity(newactivity);
+            };
+
+
+            btnScores = FindViewById<Button>(Resource.Id.btnScores);
+            btnScores.Click += delegate
+            {
+                var newactivity = new Intent(this, typeof(ScoresActivity));
+
+                newactivity.PutExtra("Activity_Name", "Scores Activity");
+                StartActivity(newactivity);
+            };
+        }
+    }
+}
+
---
+++ CODE/V1.1/XamarinGame/XamarinGame.Droid/ProfileActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace XamarinGame.Droid
+{
+    [Activity(Label = "Profile Activity")]
+    public class ProfileActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.profile_layout);
+        }
+    }
+}
