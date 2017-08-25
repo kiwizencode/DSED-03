234045c14e1490a371e94063180d2fc031ce6907
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-08-26 08:14:33 +1200

    create new folder V0.3

--- Rx/V0.2/HangmanApp/HangmanApp.Droid/Activities/Activity_Game.cs
+++ Rx/V0.2/HangmanApp/HangmanApp.Droid/Activities/Activity_Game.cs
@@ -14,6 +14,7 @@ using ReactiveUI;
 
 using System.Threading;
 using HangmanApp.Droid.ViewModel;
+using Android.Graphics;
 
 namespace HangmanApp.Droid.Activities
 {
@@ -79,52 +80,24 @@ namespace HangmanApp.Droid.Activities
         }
 
 
-        private void SetButton(int id, string resource)
-        {
-            var button = FindViewById<ImageButton>(id);
-            int image_id = Resources.GetIdentifier(resource, "drawable", PackageName);
 
-            //button.SetBackgroundResource(image_id);
-            button.SetImageResource(image_id);
-            button.SetScaleType(ImageView.ScaleType.FitXy);
-            
-        }
-        private string Btn01
-        {
-            get => string.Empty;
-            set => SetButton(Resource.Id.btn01, value);
-        }
 
-        private string Btn02
-        {
-            get => string.Empty;
-            set => SetButton(Resource.Id.btn02, value);
-        }
 
-        private string Btn03
-        {
-            get => string.Empty;
-            set => SetButton(Resource.Id.btn03, value);
-        }
-
-        private string Btn04
-        {
-            get => string.Empty;
-            set => SetButton(Resource.Id.btn04, value);
-        }
-
-        private string Btn05
-        {
-            get => string.Empty;
-            set => SetButton(Resource.Id.btn05, value);
-        }
-
-        private string Btn06
-        {
-            get => string.Empty;
-            set => SetButton(Resource.Id.btn06, value);
-        }
-
+        private Button btn01;
+        private Button btn02;
+        private Button btn03;
+        private Button btn04;
+        private Button btn05;
+        private Button btn06;
+        private Button btn07;
+        private Button btn08;
+        private Button btn09;
+        private Button btn10;
+        private Button btn11;
+        private Button btn12;
+        private Button btn13;
+        private Button btn14;
+        private Button btn15;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -152,12 +125,159 @@ namespace HangmanApp.Droid.Activities
             /*
              * The following code bind the keyboard to the View Model
              */
-            this.OneWayBind(ViewModel, x => x.Btn01, c => c.Btn01);
-            this.OneWayBind(ViewModel, x => x.Btn02, c => c.Btn02);
-            this.OneWayBind(ViewModel, x => x.Btn03, c => c.Btn03);
-            this.OneWayBind(ViewModel, x => x.Btn04, c => c.Btn04);
-            this.OneWayBind(ViewModel, x => x.Btn05, c => c.Btn05);
-            this.OneWayBind(ViewModel, x => x.Btn06, c => c.Btn06);
+
+
+                /*
+                 * 
+                 */
+                void SetupButton(out Button btn, int btn_id)
+                {
+                    btn = FindViewById<Button>(btn_id);
+                    Typeface tf = Typeface.CreateFromAsset(Assets, "fonts/FFF_Tusj.ttf");
+                    btn.Typeface = tf;
+                }
+
+            SetupButton(out this.btn01, Resource.Id.btn01);
+            SetupButton(out this.btn02, Resource.Id.btn02);
+            SetupButton(out this.btn03, Resource.Id.btn03);
+            SetupButton(out this.btn04, Resource.Id.btn04);
+            SetupButton(out this.btn05, Resource.Id.btn05);
+            SetupButton(out this.btn06, Resource.Id.btn06);
+            SetupButton(out this.btn07, Resource.Id.btn07);
+            SetupButton(out this.btn08, Resource.Id.btn08);
+            SetupButton(out this.btn09, Resource.Id.btn09);
+            SetupButton(out this.btn10, Resource.Id.btn10);
+            SetupButton(out this.btn11, Resource.Id.btn11);
+            SetupButton(out this.btn12, Resource.Id.btn12);
+            SetupButton(out this.btn13, Resource.Id.btn13);
+            SetupButton(out this.btn14, Resource.Id.btn14);
+            SetupButton(out this.btn15, Resource.Id.btn15);
+
+            this.OneWayBind(ViewModel, x => x.Btn01, c => c.btn01.Text);
+            this.OneWayBind(ViewModel, x => x.Btn02, c => c.btn02.Text);
+            this.OneWayBind(ViewModel, x => x.Btn03, c => c.btn03.Text);
+            this.OneWayBind(ViewModel, x => x.Btn04, c => c.btn04.Text);
+            this.OneWayBind(ViewModel, x => x.Btn05, c => c.btn05.Text);
+            this.OneWayBind(ViewModel, x => x.Btn06, c => c.btn06.Text);
+            this.OneWayBind(ViewModel, x => x.Btn07, c => c.btn07.Text);
+            this.OneWayBind(ViewModel, x => x.Btn08, c => c.btn08.Text);
+            this.OneWayBind(ViewModel, x => x.Btn09, c => c.btn09.Text);
+            this.OneWayBind(ViewModel, x => x.Btn10, c => c.btn10.Text);
+            this.OneWayBind(ViewModel, x => x.Btn11, c => c.btn11.Text);
+            this.OneWayBind(ViewModel, x => x.Btn12, c => c.btn12.Text);
+            this.OneWayBind(ViewModel, x => x.Btn13, c => c.btn13.Text);
+            this.OneWayBind(ViewModel, x => x.Btn14, c => c.btn14.Text);
+            this.OneWayBind(ViewModel, x => x.Btn15, c => c.btn15.Text);
+
+
+                void ButtonTextUppercase(ref Button btn)
+                {
+                    var text = btn.Text;
+                    btn.Tag = text;
+                    btn.Text = text.ToUpper();
+                }
+
+            this.WhenAny(x => x.btn01.Text, _ => string.Empty).Subscribe(
+                func => { ButtonTextUppercase(ref this.btn01); });
+            this.WhenAny(x => x.btn02.Text, _ => string.Empty).Subscribe(
+                func => { ButtonTextUppercase(ref this.btn02); });
+            this.WhenAny(x => x.btn03.Text, _ => string.Empty).Subscribe(
+                func => { ButtonTextUppercase(ref this.btn03); });
+            this.WhenAny(x => x.btn04.Text, _ => string.Empty).Subscribe(
+                func => { ButtonTextUppercase(ref this.btn04); });
+            this.WhenAny(x => x.btn05.Text, _ => string.Empty).Subscribe(
+                func => { ButtonTextUppercase(ref this.btn05); });
+            this.WhenAny(x => x.btn06.Text, _ => string.Empty).Subscribe(
+                func => { ButtonTextUppercase(ref this.btn06); });
+            this.WhenAny(x => x.btn07.Text, _ => string.Empty).Subscribe(
+                func => { ButtonTextUppercase(ref this.btn07); });
+            this.WhenAny(x => x.btn08.Text, _ => string.Empty).Subscribe(
+                func => { ButtonTextUppercase(ref this.btn08); });
+            this.WhenAny(x => x.btn09.Text, _ => string.Empty).Subscribe(
+                func => { ButtonTextUppercase(ref this.btn09); });
+            this.WhenAny(x => x.btn10.Text, _ => string.Empty).Subscribe(
+                func => { ButtonTextUppercase(ref this.btn10); });
+            this.WhenAny(x => x.btn11.Text, _ => string.Empty).Subscribe(
+                func => { ButtonTextUppercase(ref this.btn11); });
+            this.WhenAny(x => x.btn12.Text, _ => string.Empty).Subscribe(
+                func => { ButtonTextUppercase(ref this.btn12); });
+            this.WhenAny(x => x.btn13.Text, _ => string.Empty).Subscribe(
+                func => { ButtonTextUppercase(ref this.btn13); });
+            this.WhenAny(x => x.btn14.Text, _ => string.Empty).Subscribe(
+                func => { ButtonTextUppercase(ref this.btn14); });
+            this.WhenAny(x => x.btn15.Text, _ => string.Empty).Subscribe(
+                func => { ButtonTextUppercase(ref this.btn15); });
+
+
+
+            //this.OneWayBind(ViewModel, x => x.Btn01, c => c.Btn01);
+            //this.OneWayBind(ViewModel, x => x.Btn02, c => c.Btn02);
+            //this.OneWayBind(ViewModel, x => x.Btn03, c => c.Btn03);
+            //this.OneWayBind(ViewModel, x => x.Btn04, c => c.Btn04);
+            //this.OneWayBind(ViewModel, x => x.Btn05, c => c.Btn05);
+            //this.OneWayBind(ViewModel, x => x.Btn06, c => c.Btn06);
+
+            //btn01 = FindViewById<Button>(Resource.Id.btn01);
+            //btn02 = FindViewById<Button>(Resource.Id.btn02);
+            //btn03 = FindViewById<Button>(Resource.Id.btn03);
+            //btn04 = FindViewById<Button>(Resource.Id.btn04);
+            //btn05 = FindViewById<Button>(Resource.Id.btn05);
+            //btn06 = FindViewById<Button>(Resource.Id.btn06);
+            //btn07 = FindViewById<Button>(Resource.Id.btn07);
+            //btn08 = FindViewById<Button>(Resource.Id.btn08);
+            //btn09 = FindViewById<Button>(Resource.Id.btn09);
+            //btn10 = FindViewById<Button>(Resource.Id.btn10);
+            //btn11 = FindViewById<Button>(Resource.Id.btn11);
+            //btn12 = FindViewById<Button>(Resource.Id.btn12);
+            //btn13 = FindViewById<Button>(Resource.Id.btn13);
+            //btn14 = FindViewById<Button>(Resource.Id.btn14);
+            //btn15 = FindViewById<Button>(Resource.Id.btn15);
+
         }
+
+        //private void SetButton(int id, string text)
+        //{
+        //    var button = FindViewById<Button>(id);
+        //    button.Text = text;
+        //    //int image_id = Resources.GetIdentifier(text, "drawable", PackageName);
+        //    //button.SetBackgroundResource(image_id);
+        //    //button.SetImageResource(image_id);
+        //    //button.SetScaleType(ImageView.ScaleType.FitXy);
+        //}
+        //private string Btn01
+        //{
+        //    get => string.Empty;
+        //    set => SetButton(Resource.Id.btn01, value);
+        //}
+
+        //private string Btn02
+        //{
+        //    get => string.Empty;
+        //    set => SetButton(Resource.Id.btn02, value);
+        //}
+
+        //private string Btn03
+        //{
+        //    get => string.Empty;
+        //    set => SetButton(Resource.Id.btn03, value);
+        //}
+
+        //private string Btn04
+        //{
+        //    get => string.Empty;
+        //    set => SetButton(Resource.Id.btn04, value);
+        //}
+
+        //private string Btn05
+        //{
+        //    get => string.Empty;
+        //    set => SetButton(Resource.Id.btn05, value);
+        //}
+
+        //private string Btn06
+        //{
+        //    get => string.Empty;
+        //    set => SetButton(Resource.Id.btn06, value);
+        //}
     }
 }
--- Rx/V0.2/HangmanApp/HangmanApp.Droid/ViewModel/ViewModel_Game.cs
+++ Rx/V0.2/HangmanApp/HangmanApp.Droid/ViewModel/ViewModel_Game.cs
@@ -56,48 +56,107 @@ namespace HangmanApp.Droid.ViewModel
         }
 
 
-
-        private string _btn01 = BlankFile;
+        private string _btn01 = string.Empty;
         public string Btn01
         {
             get => _btn01;
-            set => this.RaiseAndSetIfChanged(ref _btn01, LetterFile + value);
+            set => this.RaiseAndSetIfChanged(ref _btn01, value);
         }
 
-        private string _btn02 = BlankFile;
+        private string _btn02 = string.Empty;
         public string Btn02
         {
             get => _btn02;
-            set => this.RaiseAndSetIfChanged(ref _btn02, LetterFile + value);
+            set => this.RaiseAndSetIfChanged(ref _btn02, value);
         }
 
-        private string _btn03 = BlankFile;
+        private string _btn03 = string.Empty;
         public string Btn03
         {
             get => _btn03;
-            set => this.RaiseAndSetIfChanged(ref _btn03, LetterFile + value);
+            set => this.RaiseAndSetIfChanged(ref _btn03, value);
         }
-        private string _btn04 = BlankFile;
+        private string _btn04 = string.Empty;
         public string Btn04
         {
             get => _btn04;
-            set => this.RaiseAndSetIfChanged(ref _btn04, LetterFile + value);
+            set => this.RaiseAndSetIfChanged(ref _btn04, value);
         }
 
-        private string _btn05 = BlankFile;
+        private string _btn05 = string.Empty;
         public string Btn05
         {
             get => _btn05;
-            set => this.RaiseAndSetIfChanged(ref _btn05, LetterFile + value);
+            set => this.RaiseAndSetIfChanged(ref _btn05, value);
         }
 
-        private string _btn06 = BlankFile;
+        private string _btn06 = string.Empty;
         public string Btn06
         {
             get => _btn06;
-            set => this.RaiseAndSetIfChanged(ref _btn06, LetterFile + value);
+            set => this.RaiseAndSetIfChanged(ref _btn06, value);
         }
 
+        private string _btn07 = string.Empty;
+        public string Btn07
+        {
+            get => _btn07;
+            set => this.RaiseAndSetIfChanged(ref _btn07, value);
+        }
+
+        private string _btn08 = string.Empty;
+        public string Btn08
+        {
+            get => _btn08;
+            set => this.RaiseAndSetIfChanged(ref _btn08, value);
+        }
+
+        private string _btn09 = string.Empty;
+        public string Btn09
+        {
+            get => _btn09;
+            set => this.RaiseAndSetIfChanged(ref _btn09, value);
+        }
+        private string _btn10 = string.Empty;
+        public string Btn10
+        {
+            get => _btn10;
+            set => this.RaiseAndSetIfChanged(ref _btn10, value);
+        }
+
+        private string _btn11 = string.Empty;
+        public string Btn11
+        {
+            get => _btn11;
+            set => this.RaiseAndSetIfChanged(ref _btn11, value);
+        }
+
+        private string _btn12 = string.Empty;
+        public string Btn12
+        {
+            get => _btn12;
+            set => this.RaiseAndSetIfChanged(ref _btn12, value);
+        }
+
+        private string _btn13 = string.Empty;
+        public string Btn13
+        {
+            get => _btn13;
+            set => this.RaiseAndSetIfChanged(ref _btn13, value);
+        }
+        private string _btn14 = string.Empty;
+        public string Btn14
+        {
+            get => _btn14;
+            set => this.RaiseAndSetIfChanged(ref _btn14, value);
+        }
+
+        private string _btn15 = string.Empty;
+        public string Btn15
+        {
+            get => _btn15;
+            set => this.RaiseAndSetIfChanged(ref _btn15, value);
+        }
         public ViewModel_Game()
         {
             ShowHiddenWord();
@@ -110,15 +169,24 @@ namespace HangmanApp.Droid.ViewModel
             string random_letter = WordsHelper.GenerateRandomLetter(hidden_word);
             for(int i=0; i < 15;i++)
             {
-                string value = LetterFile + random_letter[i];
-                switch (i)
+                string value = ""+random_letter[i];
+                switch (i+1)
                 {
-                    case 1: this.RaiseAndSetIfChanged(ref _btn01, value); break;
-                    case 2: this.RaiseAndSetIfChanged(ref _btn02, value); break;
-                    case 3: this.RaiseAndSetIfChanged(ref _btn03, value); break;
-                    case 4: this.RaiseAndSetIfChanged(ref _btn04, value); break;
-                    case 5: this.RaiseAndSetIfChanged(ref _btn05, value); break;
-                    case 6: this.RaiseAndSetIfChanged(ref _btn06, value); break;
+                    case 1: Btn01 = value; break;
+                    case 2: Btn02 = value; break;
+                    case 3: Btn03 = value; break;
+                    case 4: Btn04 = value; break;
+                    case 5: Btn05 = value; break;
+                    case 6: Btn06 = value; break;
+                    case 7: Btn07 = value; break;
+                    case 8: Btn08 = value; break;
+                    case 9: Btn09 = value; break;
+                    case 10: Btn10 = value; break;
+                    case 11: Btn11 = value; break;
+                    case 12: Btn12 = value; break;
+                    case 13: Btn13 = value; break;
+                    case 14: Btn14 = value; break;
+                    case 15: Btn15 = value; break;
                 }
             }
         }
