266d964f11ed3c4a9b494469438cde1a90a16b12
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-09-11 14:18:42 +1200

    save changes

--- Rx/v0.5/HangmanApp/HangmanApp.Droid/Resources/Resource.Designer.cs
+++ Rx/v0.5/HangmanApp/HangmanApp.Droid/Resources/Resource.Designer.cs
@@ -2331,26 +2331,26 @@ namespace HangmanApp.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f070079
-			public const int ImageSlot01 = 2131165305;
-			
 			// aapt resource value: 0x7f07007a
-			public const int ImageSlot02 = 2131165306;
+			public const int ImageSlot01 = 2131165306;
 			
 			// aapt resource value: 0x7f07007b
-			public const int ImageSlot03 = 2131165307;
+			public const int ImageSlot02 = 2131165307;
 			
 			// aapt resource value: 0x7f07007c
-			public const int ImageSlot04 = 2131165308;
+			public const int ImageSlot03 = 2131165308;
 			
 			// aapt resource value: 0x7f07007d
-			public const int ImageSlot05 = 2131165309;
+			public const int ImageSlot04 = 2131165309;
+			
+			// aapt resource value: 0x7f07007e
+			public const int ImageSlot05 = 2131165310;
 			
 			// aapt resource value: 0x7f070072
 			public const int TitleLayout = 2131165298;
 			
-			// aapt resource value: 0x7f0700a6
-			public const int action0 = 2131165350;
+			// aapt resource value: 0x7f0700a7
+			public const int action0 = 2131165351;
 			
 			// aapt resource value: 0x7f07005a
 			public const int action_bar = 2131165274;
@@ -2376,8 +2376,8 @@ namespace HangmanApp.Droid
 			// aapt resource value: 0x7f07005b
 			public const int action_context_bar = 2131165275;
 			
-			// aapt resource value: 0x7f0700aa
-			public const int action_divider = 2131165354;
+			// aapt resource value: 0x7f0700ab
+			public const int action_divider = 2131165355;
 			
 			// aapt resource value: 0x7f070003
 			public const int action_menu_divider = 2131165187;
@@ -2409,56 +2409,59 @@ namespace HangmanApp.Droid
 			// aapt resource value: 0x7f07002a
 			public const int bottom = 2131165226;
 			
-			// aapt resource value: 0x7f07007e
-			public const int btn01 = 2131165310;
-			
 			// aapt resource value: 0x7f07007f
-			public const int btn02 = 2131165311;
+			public const int btn01 = 2131165311;
 			
 			// aapt resource value: 0x7f070080
-			public const int btn03 = 2131165312;
+			public const int btn02 = 2131165312;
 			
 			// aapt resource value: 0x7f070081
-			public const int btn04 = 2131165313;
+			public const int btn03 = 2131165313;
 			
 			// aapt resource value: 0x7f070082
-			public const int btn05 = 2131165314;
+			public const int btn04 = 2131165314;
 			
 			// aapt resource value: 0x7f070083
-			public const int btn06 = 2131165315;
+			public const int btn05 = 2131165315;
 			
 			// aapt resource value: 0x7f070084
-			public const int btn07 = 2131165316;
+			public const int btn06 = 2131165316;
 			
 			// aapt resource value: 0x7f070085
-			public const int btn08 = 2131165317;
+			public const int btn07 = 2131165317;
 			
 			// aapt resource value: 0x7f070086
-			public const int btn09 = 2131165318;
+			public const int btn08 = 2131165318;
 			
 			// aapt resource value: 0x7f070087
-			public const int btn10 = 2131165319;
+			public const int btn09 = 2131165319;
 			
 			// aapt resource value: 0x7f070088
-			public const int btn11 = 2131165320;
+			public const int btn10 = 2131165320;
 			
 			// aapt resource value: 0x7f070089
-			public const int btn12 = 2131165321;
+			public const int btn11 = 2131165321;
 			
 			// aapt resource value: 0x7f07008a
-			public const int btn13 = 2131165322;
+			public const int btn12 = 2131165322;
 			
 			// aapt resource value: 0x7f07008b
-			public const int btn14 = 2131165323;
+			public const int btn13 = 2131165323;
 			
 			// aapt resource value: 0x7f07008c
-			public const int btn15 = 2131165324;
+			public const int btn14 = 2131165324;
+			
+			// aapt resource value: 0x7f07008d
+			public const int btn15 = 2131165325;
+			
+			// aapt resource value: 0x7f070079
+			public const int btnStartNew = 2131165305;
 			
 			// aapt resource value: 0x7f070044
 			public const int buttonPanel = 2131165252;
 			
-			// aapt resource value: 0x7f0700a7
-			public const int cancel_action = 2131165351;
+			// aapt resource value: 0x7f0700a8
+			public const int cancel_action = 2131165352;
 			
 			// aapt resource value: 0x7f07002b
 			public const int center = 2131165227;
@@ -2472,8 +2475,8 @@ namespace HangmanApp.Droid
 			// aapt resource value: 0x7f070052
 			public const int checkbox = 2131165266;
 			
-			// aapt resource value: 0x7f0700ad
-			public const int chronometer = 2131165357;
+			// aapt resource value: 0x7f0700ae
+			public const int chronometer = 2131165358;
 			
 			// aapt resource value: 0x7f070033
 			public const int clip_horizontal = 2131165235;
@@ -2523,8 +2526,8 @@ namespace HangmanApp.Droid
 			// aapt resource value: 0x7f07001c
 			public const int end = 2131165212;
 			
-			// aapt resource value: 0x7f0700b2
-			public const int end_padder = 2131165362;
+			// aapt resource value: 0x7f0700b3
+			public const int end_padder = 2131165363;
 			
 			// aapt resource value: 0x7f070023
 			public const int enterAlways = 2131165219;
@@ -2571,8 +2574,8 @@ namespace HangmanApp.Droid
 			// aapt resource value: 0x7f070078
 			public const int imageViewHangman = 2131165304;
 			
-			// aapt resource value: 0x7f0700b1
-			public const int info = 2131165361;
+			// aapt resource value: 0x7f0700b2
+			public const int info = 2131165362;
 			
 			// aapt resource value: 0x7f070000
 			public const int item_touch_helper_previous_elevation = 2131165184;
@@ -2580,11 +2583,11 @@ namespace HangmanApp.Droid
 			// aapt resource value: 0x7f07002f
 			public const int left = 2131165231;
 			
-			// aapt resource value: 0x7f0700ab
-			public const int line1 = 2131165355;
+			// aapt resource value: 0x7f0700ac
+			public const int line1 = 2131165356;
 			
-			// aapt resource value: 0x7f0700af
-			public const int line3 = 2131165359;
+			// aapt resource value: 0x7f0700b0
+			public const int line3 = 2131165360;
 			
 			// aapt resource value: 0x7f07000b
 			public const int listMode = 2131165195;
@@ -2592,8 +2595,8 @@ namespace HangmanApp.Droid
 			// aapt resource value: 0x7f070041
 			public const int list_item = 2131165249;
 			
-			// aapt resource value: 0x7f0700a9
-			public const int media_actions = 2131165353;
+			// aapt resource value: 0x7f0700aa
+			public const int media_actions = 2131165354;
 			
 			// aapt resource value: 0x7f07001d
 			public const int middle = 2131165213;
@@ -2601,77 +2604,77 @@ namespace HangmanApp.Droid
 			// aapt resource value: 0x7f070037
 			public const int mini = 2131165239;
 			
-			// aapt resource value: 0x7f070098
-			public const int mr_art = 2131165336;
-			
-			// aapt resource value: 0x7f07008d
-			public const int mr_chooser_list = 2131165325;
-			
-			// aapt resource value: 0x7f070090
-			public const int mr_chooser_route_desc = 2131165328;
+			// aapt resource value: 0x7f070099
+			public const int mr_art = 2131165337;
 			
 			// aapt resource value: 0x7f07008e
-			public const int mr_chooser_route_icon = 2131165326;
-			
-			// aapt resource value: 0x7f07008f
-			public const int mr_chooser_route_name = 2131165327;
-			
-			// aapt resource value: 0x7f070095
-			public const int mr_close = 2131165333;
-			
-			// aapt resource value: 0x7f07009b
-			public const int mr_control_divider = 2131165339;
-			
-			// aapt resource value: 0x7f0700a1
-			public const int mr_control_play_pause = 2131165345;
-			
-			// aapt resource value: 0x7f0700a4
-			public const int mr_control_subtitle = 2131165348;
-			
-			// aapt resource value: 0x7f0700a3
-			public const int mr_control_title = 2131165347;
-			
-			// aapt resource value: 0x7f0700a2
-			public const int mr_control_title_container = 2131165346;
-			
-			// aapt resource value: 0x7f070096
-			public const int mr_custom_control = 2131165334;
-			
-			// aapt resource value: 0x7f070097
-			public const int mr_default_control = 2131165335;
-			
-			// aapt resource value: 0x7f070092
-			public const int mr_dialog_area = 2131165330;
+			public const int mr_chooser_list = 2131165326;
 			
 			// aapt resource value: 0x7f070091
-			public const int mr_expandable_area = 2131165329;
+			public const int mr_chooser_route_desc = 2131165329;
 			
-			// aapt resource value: 0x7f0700a5
-			public const int mr_group_expand_collapse = 2131165349;
+			// aapt resource value: 0x7f07008f
+			public const int mr_chooser_route_icon = 2131165327;
 			
-			// aapt resource value: 0x7f070099
-			public const int mr_media_main_control = 2131165337;
+			// aapt resource value: 0x7f070090
+			public const int mr_chooser_route_name = 2131165328;
 			
-			// aapt resource value: 0x7f070094
-			public const int mr_name = 2131165332;
-			
-			// aapt resource value: 0x7f07009a
-			public const int mr_playback_control = 2131165338;
-			
-			// aapt resource value: 0x7f070093
-			public const int mr_title_bar = 2131165331;
+			// aapt resource value: 0x7f070096
+			public const int mr_close = 2131165334;
 			
 			// aapt resource value: 0x7f07009c
-			public const int mr_volume_control = 2131165340;
+			public const int mr_control_divider = 2131165340;
+			
+			// aapt resource value: 0x7f0700a2
+			public const int mr_control_play_pause = 2131165346;
+			
+			// aapt resource value: 0x7f0700a5
+			public const int mr_control_subtitle = 2131165349;
+			
+			// aapt resource value: 0x7f0700a4
+			public const int mr_control_title = 2131165348;
+			
+			// aapt resource value: 0x7f0700a3
+			public const int mr_control_title_container = 2131165347;
+			
+			// aapt resource value: 0x7f070097
+			public const int mr_custom_control = 2131165335;
+			
+			// aapt resource value: 0x7f070098
+			public const int mr_default_control = 2131165336;
+			
+			// aapt resource value: 0x7f070093
+			public const int mr_dialog_area = 2131165331;
+			
+			// aapt resource value: 0x7f070092
+			public const int mr_expandable_area = 2131165330;
+			
+			// aapt resource value: 0x7f0700a6
+			public const int mr_group_expand_collapse = 2131165350;
+			
+			// aapt resource value: 0x7f07009a
+			public const int mr_media_main_control = 2131165338;
+			
+			// aapt resource value: 0x7f070095
+			public const int mr_name = 2131165333;
+			
+			// aapt resource value: 0x7f07009b
+			public const int mr_playback_control = 2131165339;
+			
+			// aapt resource value: 0x7f070094
+			public const int mr_title_bar = 2131165332;
 			
 			// aapt resource value: 0x7f07009d
-			public const int mr_volume_group_list = 2131165341;
+			public const int mr_volume_control = 2131165341;
 			
-			// aapt resource value: 0x7f07009f
-			public const int mr_volume_item_icon = 2131165343;
+			// aapt resource value: 0x7f07009e
+			public const int mr_volume_group_list = 2131165342;
 			
 			// aapt resource value: 0x7f0700a0
-			public const int mr_volume_slider = 2131165344;
+			public const int mr_volume_item_icon = 2131165344;
+			
+			// aapt resource value: 0x7f0700a1
+			public const int mr_volume_slider = 2131165345;
 			
 			// aapt resource value: 0x7f070016
 			public const int multiply = 2131165206;
@@ -2799,8 +2802,8 @@ namespace HangmanApp.Droid
 			// aapt resource value: 0x7f070031
 			public const int start = 2131165233;
 			
-			// aapt resource value: 0x7f0700a8
-			public const int status_bar_latest_event_content = 2131165352;
+			// aapt resource value: 0x7f0700a9
+			public const int status_bar_latest_event_content = 2131165353;
 			
 			// aapt resource value: 0x7f070065
 			public const int submit_area = 2131165285;
@@ -2808,11 +2811,11 @@ namespace HangmanApp.Droid
 			// aapt resource value: 0x7f07000d
 			public const int tabMode = 2131165197;
 			
-			// aapt resource value: 0x7f0700b0
-			public const int text = 2131165360;
+			// aapt resource value: 0x7f0700b1
+			public const int text = 2131165361;
 			
-			// aapt resource value: 0x7f0700ae
-			public const int text2 = 2131165358;
+			// aapt resource value: 0x7f0700af
+			public const int text2 = 2131165359;
 			
 			// aapt resource value: 0x7f07004d
 			public const int textSpacerNoButtons = 2131165261;
@@ -2832,8 +2835,8 @@ namespace HangmanApp.Droid
 			// aapt resource value: 0x7f070073
 			public const int textViewTitle = 2131165299;
 			
-			// aapt resource value: 0x7f0700ac
-			public const int time = 2131165356;
+			// aapt resource value: 0x7f0700ad
+			public const int time = 2131165357;
 			
 			// aapt resource value: 0x7f070043
 			public const int title = 2131165251;
@@ -2859,8 +2862,8 @@ namespace HangmanApp.Droid
 			// aapt resource value: 0x7f07000a
 			public const int view_offset_helper = 2131165194;
 			
-			// aapt resource value: 0x7f07009e
-			public const int volume_item_container = 2131165342;
+			// aapt resource value: 0x7f07009f
+			public const int volume_item_container = 2131165343;
 			
 			// aapt resource value: 0x7f070022
 			public const int withText = 2131165218;
