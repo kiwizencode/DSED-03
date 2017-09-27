5d4e994614bce79fa66d00df3acd3067297c013b
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-09-28 11:47:16 +1300

    create a shared project and update all c# source code

---
+++ Guess5App/Guess5App.Shared/Data/ProfileDatabase.cs
@@ -0,0 +1,77 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using SQLite;
+
+using HangmanApp.Shared.Model;
+
+
+namespace HangmanApp.Shared.DataAccessObject
+{
+    public class ProfileDatabase : SQLiteConnection
+    {
+        static object locker = new object();
+        /// <summary> Initializes a new instance of the <see cref="HangmanApp.Shared.DataAccessObject.ProfileDB"/> TaskDatabase.
+        /// if the database doesn't exist, it will create the database and the all tables. </summary>
+        /// <param name="path">Path.</param>
+        public ProfileDatabase(string path) : base(path)
+        {
+            // create the tables
+            CreateTable<Model_Profile>();
+        }
+
+        /// <summary>return list of profile</summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public IEnumerable<T> GetItems<T>() where T : IModel, new()
+        {
+            lock (locker)
+            {
+                return (from i in Table<T>() select i).ToList();
+            }
+        }
+
+        /// <summary> retreive profile by providing the id.</summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="id">profile ID</param>
+        /// <returns></returns>
+        public T GetItem<T>(int id) where T : IModel, new()
+        {
+            lock (locker)
+            {
+                return Table<T>().FirstOrDefault(x => x.ID == id);
+            }
+        }
+
+        /// <summary> save profile data. If ID is 0, create a new record, else update record by ID</summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public int SaveItem<T>(T item) where T : IModel
+        {
+            lock (locker)
+            {
+                if (item.ID != 0)
+                {
+                    Update(item);
+                    return item.ID;
+                }
+                else
+                {
+                    return Insert(item);
+                }
+            }
+        }
+
+        /// <summary>delete a record by providing the profile ID</summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public int DeleteItem<T>(int id) where T : IModel, new()
+        {
+            lock (locker)
+            {
+                return Delete(new T() { ID = id });
+            }
+        }
+    }
+}
---
+++ Guess5App/Guess5App.Shared/Model/IModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HangmanApp.Shared.Model
+{
+    public interface IModel
+    {
+        int ID { get; set; }
+    }
+}
